9aa988fd745a62c8ec580066e3dacd3fd9835642
Srushti-25 <Srushtidaware143@gmail.com>
2026-07-27 20:41:14 +0530

    Update CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: ["http://localhost:5173",
+                   "https://to-do-app-lgzc-r1ro4panp-srushti-25s-projects.vercel.app"
+        ],
         credentials: true,
     })
 );
